f518e6111c247278a5de355cbc869a7abf5f496f
SURFACE-LM\lukem <luke.munderich@web.de>
2022-02-11 16:49:04 +0100

    Bild in die Python-Datei eingefügt

--- fenster.py
+++ fenster.py
@@ -1,6 +1,10 @@
 from tkinter import *
+from PIL import ImageTk, Image
 fenster = Tk()
 fenster.geometry("500x500")
 text=Label(text="Test")
 text.pack(expand=1)
+bild = ImageTk.PhotoImage(Image.open("Bilder\König_schwarz.png"))
+bildInFenster = Label(image=bild)
+bildInFenster.pack()
 fenster.mainloop()
