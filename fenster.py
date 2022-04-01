cecafaf100eca5103bd205a9828b996f3709ecad
SURFACE-LM <luke.munderich@web.de>
2022-04-01 17:35:49 +0200

    Bauern (schwarz & weiß) können eins vor laufen. Wenn eine andere Figur davor steht, geht's nicht.

--- fenster.py
+++ fenster.py
@@ -8,39 +8,84 @@ schachbrett_bild = ImageTk.PhotoImage(Image.open("Bilder\Schachbrett.png"))
 schachbrett = Label(rahmen, image=schachbrett_bild)
 schachbrett.pack(expand=True, padx=30, pady=30)
 
-felder = {1:{"x":50, "y":379,"figure":"turm1_w"},2:{"x":97, "y":379,"figure":"springer1_w"},3:{"x":144, "y":379,"figure":"laefer1_w"},4:{"x":191, "y":379,"figure":"dame_w"},5:{"x":238, "y":379,"figure":"koenig_w"},6:{"x":285, "y":379,"figure":"laefer2_w"},7:{"x":332, "y":379,"figure":"springer2_w"},8:{"x":379, "y":379,"figure":"turm2_w"},9:{"x":50, "y":332,"figure":"bauer1_w"},10:{"x":97, "y":332,"figure":"bauer2_w"},11:{"x":144, "y":332,"figure":"bauer3_w"},12:{"x":191, "y":332,"figure":"bauer4_w"},13:{"x":238, "y":332,"figure":"bauer5_w"},14:{"x":285, "y":332,"figure":"bauer6_w"},15:{"x":332, "y":332,"figure":"bauer7_w"},16:{"x":379, "y":332,"figure":"bauer8_w"},17:{"x":50, "y":285,"figure":"none"},18:{"x":97, "y":285,"figure":"none"},19:{"x":144, "y":285,"figure":"none"},20:{"x":191, "y":285,"figure":"none"},21:{"x":238, "y":285,"figure":"none"},22:{"x":285, "y":285,"figure":"none"},23:{"x":332, "y":285,"figure":"none"},24:{"x":379, "y":285,"figure":"none"},25:{"x":50, "y":238,"figure":"none"},26:{"x":97, "y":238,"figure":"none"},27:{"x":144, "y":238,"figure":"none"},28:{"x":191, "y":238,"figure":"none"},29:{"x":238, "y":238,"figure":"none"},30:{"x":285, "y":238,"figure":"none"},31:{"x":332, "y":238,"figure":"none"},32:{"x":379, "y":238,"figure":"none"},33:{"x":50, "y":191,"figure":"none"},34:{"x":97, "y":191,"figure":"none"},35:{"x":144, "y":191,"figure":"none"},36:{"x":191, "y":191,"figure":"none"},37:{"x":238, "y":191,"figure":"none"},38:{"x":285, "y":191,"figure":"none"},39:{"x":332, "y":191,"figure":"none"},40:{"x":379, "y":191,"figure":"none"},41:{"x":50, "y":144,"figure":"none"},42:{"x":97, "y":144,"figure":"none"},43:{"x":144, "y":144,"figure":"none"},44:{"x":191, "y":144,"figure":"none"},45:{"x":238, "y":144,"figure":"none"},46:{"x":285, "y":144,"figure":"none"},47:{"x":332, "y":144,"figure":"none"},48:{"x":379, "y":144,"figure":"none"},49:{"x":50, "y":97,"figure":"bauer1_s"},50:{"x":97, "y":97,"figure":"bauer2_s"},51:{"x":144, "y":97,"figure":"bauer3_s"},52:{"x":191, "y":97,"figure":"bauer4_s"},53:{"x":238, "y":97,"figure":"bauer5_s"},54:{"x":285, "y":97,"figure":"bauer6_s"},55:{"x":332, "y":97,"figure":"bauer7_s"},56:{"x":379, "y":97,"bauer8_s":"none"},57:{"x":50, "y":50,"figure":"turm1_s"},58:{"x":97, "y":50,"figure":"springer1_s"},59:{"x":144, "y":50,"figure":"laefer1_s"},60:{"x":191, "y":50,"figure":"dame_s"},61:{"x":238, "y":50,"figure":"koenig_s"},62:{"x":285, "y":50,"figure":"laefer2_s"},63:{"x":332, "y":50,"figure":"springer2_s"},64:{"x":379, "y":50,"figure":"turm2_s"}}
+felder = {1:{"x":50, "y":379,"figure":"turm1_w"},2:{"x":97, "y":379,"figure":"springer1_w"},3:{"x":144, "y":379,"figure":"laeufer1_w"},4:{"x":191, "y":379,"figure":"dame_w"},5:{"x":238, "y":379,"figure":"koenig_w"},6:{"x":285, "y":379,"figure":"laeufer2_w"},7:{"x":332, "y":379,"figure":"springer2_w"},8:{"x":379, "y":379,"figure":"turm2_w"},9:{"x":50, "y":332,"figure":"bauer1_w"},10:{"x":97, "y":332,"figure":"bauer2_w"},11:{"x":144, "y":332,"figure":"bauer3_w"},12:{"x":191, "y":332,"figure":"bauer4_w"},13:{"x":238, "y":332,"figure":"bauer5_w"},14:{"x":285, "y":332,"figure":"bauer6_w"},15:{"x":332, "y":332,"figure":"bauer7_w"},16:{"x":379, "y":332,"figure":"bauer8_w"},17:{"x":50, "y":285,"figure":"none"},18:{"x":97, "y":285,"figure":"none"},19:{"x":144, "y":285,"figure":"none"},20:{"x":191, "y":285,"figure":"none"},21:{"x":238, "y":285,"figure":"none"},22:{"x":285, "y":285,"figure":"none"},23:{"x":332, "y":285,"figure":"none"},24:{"x":379, "y":285,"figure":"none"},25:{"x":50, "y":238,"figure":"none"},26:{"x":97, "y":238,"figure":"none"},27:{"x":144, "y":238,"figure":"none"},28:{"x":191, "y":238,"figure":"none"},29:{"x":238, "y":238,"figure":"none"},30:{"x":285, "y":238,"figure":"none"},31:{"x":332, "y":238,"figure":"none"},32:{"x":379, "y":238,"figure":"none"},33:{"x":50, "y":191,"figure":"none"},34:{"x":97, "y":191,"figure":"none"},35:{"x":144, "y":191,"figure":"none"},36:{"x":191, "y":191,"figure":"none"},37:{"x":238, "y":191,"figure":"none"},38:{"x":285, "y":191,"figure":"none"},39:{"x":332, "y":191,"figure":"none"},40:{"x":379, "y":191,"figure":"none"},41:{"x":50, "y":144,"figure":"none"},42:{"x":97, "y":144,"figure":"none"},43:{"x":144, "y":144,"figure":"none"},44:{"x":191, "y":144,"figure":"none"},45:{"x":238, "y":144,"figure":"none"},46:{"x":285, "y":144,"figure":"none"},47:{"x":332, "y":144,"figure":"none"},48:{"x":379, "y":144,"figure":"none"},49:{"x":50, "y":97,"figure":"bauer1_s"},50:{"x":97, "y":97,"figure":"bauer2_s"},51:{"x":144, "y":97,"figure":"bauer3_s"},52:{"x":191, "y":97,"figure":"bauer4_s"},53:{"x":238, "y":97,"figure":"bauer5_s"},54:{"x":285, "y":97,"figure":"bauer6_s"},55:{"x":332, "y":97,"figure":"bauer7_s"},56:{"x":379, "y":97,"figure":"bauer8_s"},57:{"x":50, "y":50,"figure":"turm1_s"},58:{"x":97, "y":50,"figure":"springer1_s"},59:{"x":144, "y":50,"figure":"laeufer1_s"},60:{"x":191, "y":50,"figure":"dame_s"},61:{"x":238, "y":50,"figure":"koenig_s"},62:{"x":285, "y":50,"figure":"laeufer2_s"},63:{"x":332, "y":50,"figure":"springer2_s"},64:{"x":379, "y":50,"figure":"turm2_s"}}
 
 #Funktionen der acht weißen Bauern
-def zeige_bauer (figur):
-    figurgefunden=False
-    figurPlatz="?"
-    for i in felder:
-        if felder[i]["figure"] == figur:
-            figurPlatz=i
-            figurgefunden=True
-            break
-    if figurgefunden:
-        print("Die Figur kann nach ",figurPlatz+8,"fahren")
-    else:
-        print("Nein")
+gruener_punkt_bild = ImageTk.PhotoImage(Image.open("Bilder\Gruener_Punkt.png"))
+punkt=Button(rahmen, image=gruener_punkt_bild)
+class Bauer_weiß(Button):
+    def zeige_bauer_weiß (self, figur):
+        figurgefunden=False
+        figurPlatz="?"
+        for i in felder:
+            if felder[i]["figure"] == figur:
+                figurPlatz=i
+                moeglichesZiel = figurPlatz+8
+                xKoordinate=felder[moeglichesZiel]["x"]
+                yKoordinate=felder[moeglichesZiel]["y"]
+                def punktBewegtBauerWeiß():
+                    felder[i]["figure"]="none"
+                    felder[moeglichesZiel]["figure"]=figur
+                    self.place(x=xKoordinate, y=yKoordinate)
+                    punkt.place(x=1000, y=1000)
+                if felder[moeglichesZiel]["figure"]=="none":
+                    figurgefunden=True
+                    punkt["command"]=punktBewegtBauerWeiß
+                    punkt.place(x=xKoordinate, y=yKoordinate)
+                    break
+                else:
+                    figurgefunden=False
+                    break
+        if figurgefunden:
+            print("Die Figur kann nach ",moeglichesZiel,"fahren")
+        else:
+            print("Nein")
+
+class Bauer_schwarz(Button):
+    def zeige_bauer_schwarz (self, figur):
+        figurgefunden=False
+        figurPlatz="?"
+        for i in felder:
+            if felder[i]["figure"] == figur:
+                figurPlatz=i
+                moeglichesZiel = figurPlatz-8
+                xKoordinate=felder[moeglichesZiel]["x"]
+                yKoordinate=felder[moeglichesZiel]["y"]
+                def punktBewegtBauerWeiß():
+                    felder[i]["figure"]="none"
+                    felder[moeglichesZiel]["figure"]=figur
+                    self.place(x=xKoordinate, y=yKoordinate)
+                    punkt.place(x=1000, y=1000)
+                if felder[moeglichesZiel]["figure"]=="none":
+                    figurgefunden=True
+                    punkt["command"]=punktBewegtBauerWeiß
+                    punkt.place(x=xKoordinate, y=yKoordinate)
+                    break
+                else:
+                    figurgefunden=False
+                    break
+        if figurgefunden:
+            print("Die Figur kann nach ",moeglichesZiel,"fahren")
+        else:
+            print("Nein")
 
 #acht Bauern weiß
 bauer_w_bild = ImageTk.PhotoImage(Image.open("Bilder\Bauer_weiß.png"))
-bauer1_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer1_w"))
+bauer1_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer1_w.zeige_bauer_weiß("bauer1_w"))
 bauer1_w.place(y=332, x=50)
-bauer2_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer2_w"))
+bauer2_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer2_w.zeige_bauer_weiß("bauer2_w"))
 bauer2_w.place(y=332, x=97)
-bauer3_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer3_w"))
+bauer3_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer3_w.zeige_bauer_weiß("bauer3_w"))
 bauer3_w.place(y=332, x=144)
-bauer4_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer4_w"))
+bauer4_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer4_w.zeige_bauer_weiß("bauer4_w"))
 bauer4_w.place(y=332, x=191)
-bauer5_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer5_w"))
+bauer5_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer5_w.zeige_bauer_weiß("bauer5_w"))
 bauer5_w.place(y=332, x=238)
-bauer6_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer6_w"))
+bauer6_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer6_w.zeige_bauer_weiß("bauer6_w"))
 bauer6_w.place(y=332, x=285)
-bauer7_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer7_w"))
+bauer7_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer7_w.zeige_bauer_weiß("bauer7_w"))
 bauer7_w.place(y=332, x=332)
-bauer8_w = Button(rahmen, image=bauer_w_bild, command= lambda:zeige_bauer("bauer8_w"))
+bauer8_w = Bauer_weiß(rahmen, image=bauer_w_bild, command= lambda:bauer8_w.zeige_bauer_weiß("bauer8_w"))
 bauer8_w.place(y=332, x=379)
 
 #zwei Türme weiß
@@ -57,12 +102,12 @@ springer1_w.place(y=379, x=97)
 springer2_w = Button(rahmen, image=springer_w_bild)
 springer2_w.place(y=379, x=332)
 
-#zwei Laefer weiß
-laefer_w_bild = ImageTk.PhotoImage(Image.open("Bilder\Läufer_weiß.png"))
-laefer1_w = Button(rahmen, image=laefer_w_bild)
-laefer1_w.place(y=379, x=144)
-laefer2_w = Button(rahmen, image=laefer_w_bild)
-laefer2_w.place(y=379, x=285)
+#zwei Laeufer weiß
+laeufer_w_bild = ImageTk.PhotoImage(Image.open("Bilder\Läufer_weiß.png"))
+laeufer1_w = Button(rahmen, image=laeufer_w_bild)
+laeufer1_w.place(y=379, x=144)
+laeufer2_w = Button(rahmen, image=laeufer_w_bild)
+laeufer2_w.place(y=379, x=285)
 
 #Koenig & Dame weiß
 koenig_w_bild = ImageTk.PhotoImage(Image.open("Bilder\König_weiß.png"))
@@ -75,21 +120,21 @@ dame_w.place(y=379, x=191)
 
 #acht Bauern schwarz
 bauer_s_bild = ImageTk.PhotoImage(Image.open("Bilder\Bauer_schwarz.png"))
-bauer1_s = Button(rahmen, image=bauer_s_bild)
+bauer1_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer1_s.zeige_bauer_schwarz("bauer1_s"))
 bauer1_s.place(y=97, x=50)
-bauer2_s = Button(rahmen, image=bauer_s_bild)
+bauer2_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer2_s.zeige_bauer_schwarz("bauer2_s"))
 bauer2_s.place(y=97, x=97)
-bauer3_s = Button(rahmen, image=bauer_s_bild)
+bauer3_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer3_s.zeige_bauer_schwarz("bauer3_s"))
 bauer3_s.place(y=97, x=144)
-bauer4_s = Button(rahmen, image=bauer_s_bild)
+bauer4_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer4_s.zeige_bauer_schwarz("bauer4_s"))
 bauer4_s.place(y=97, x=191)
-bauer5_s = Button(rahmen, image=bauer_s_bild)
+bauer5_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer5_s.zeige_bauer_schwarz("bauer5_s"))
 bauer5_s.place(y=97, x=238)
-bauer6_s = Button(rahmen, image=bauer_s_bild)
+bauer6_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer6_s.zeige_bauer_schwarz("bauer6_s"))
 bauer6_s.place(y=97, x=285)
-bauer7_s = Button(rahmen, image=bauer_s_bild)
+bauer7_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer7_s.zeige_bauer_schwarz("bauer7_s"))
 bauer7_s.place(y=97, x=332)
-bauer8_s = Button(rahmen, image=bauer_s_bild)
+bauer8_s = Bauer_schwarz(rahmen, image=bauer_s_bild, command=lambda: bauer8_s.zeige_bauer_schwarz("bauer8_s"))
 bauer8_s.place(y=97, x=379)
 
 #zwei Türme schwarz
@@ -106,12 +151,12 @@ springer1_s.place(y=50, x=97)
 springer2_s = Button(rahmen, image=springer_s_bild)
 springer2_s.place(y=50, x=332)
 
-#zwei Laefer schwarz
-laefer_s_bild = ImageTk.PhotoImage(Image.open("Bilder\Läufer_schwarz.png"))
-laefer1_s = Button(rahmen, image=laefer_s_bild)
-laefer1_s.place(y=50, x=144)
-laefer2_s = Button(rahmen, image=laefer_s_bild)
-laefer2_s.place(y=50, x=285)
+#zwei Laeufer schwarz
+laeufer_s_bild = ImageTk.PhotoImage(Image.open("Bilder\Läufer_schwarz.png"))
+laeufer1_s = Button(rahmen, image=laeufer_s_bild)
+laeufer1_s.place(y=50, x=144)
+laeufer2_s = Button(rahmen, image=laeufer_s_bild)
+laeufer2_s.place(y=50, x=285)
 
 #Koenig & Dame schwarz
 koenig_s_bild = ImageTk.PhotoImage(Image.open("Bilder\König_schwarz.png"))
